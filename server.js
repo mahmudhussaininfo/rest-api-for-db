121cfabcf82b6e7f11fb96e1d289b2df38f2e0e0
Mahmud <77590197+mahmudhussaininfo@users.noreply.github.com>
2022-10-18 11:47:49 +0600

    upload my api with db

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const colors = require('colors');
+const { urlencoded } = require('express');
+const env = require('dotenv').config();
+
+
+//port init
+const PORT = process.env.PORT || 5050
+
+//express init
+const app = express();
+
+//express middileware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//listen port
+app.listen(PORT, () => {
+    console.log(`your server is running on ${PORT}`.bgYellow.black);
+});
